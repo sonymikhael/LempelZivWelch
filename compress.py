8219a8101518255c521246949fc5260f9e1e7c40
sonymikhael <sonymikhael07@gmail.com>
2024-11-16 00:50:27 +0800

    Update compress.py
    remove print statements

--- compress.py
+++ compress.py
@@ -8,7 +8,6 @@ class LZWCompressor:
         return self.dictionary[w]
 
     def reset_dictionary(self):
-        print('will reset dictionary, size is already: ', len(self.dictionary))
         self.dictionary = {}
         self.dictionary_size = 0
 
@@ -28,7 +27,6 @@ class LZWCompressor:
     def decompress_binary_file(self, filename: str):
         self.reset_dictionary()
         self.fill_dictionary_decompression()
-        print('====================decompress==================')
 
         original_filename = filename.split('.lzw')[0]
         with open(filename, 'rb') as input_file, open('original-' + original_filename, 'wb') as output_file:
@@ -36,16 +34,11 @@ class LZWCompressor:
             next_code = 256
             while True:
                 rec = input_file.read(2)
-                print('rec', rec)
                 if len(rec) != 2:
-                    print('EOF encountered')
                     break
                 if not rec in self.dictionary:
-                    print('rec not in reverse_dict')
                     value = string + (string[0].to_bytes(1, byteorder='little'))
-                    print('table[', rec, '] =', value)
                     self.dictionary[rec] = value
-                print('will write', self.dictionary[rec], 'to file')
                 output_file.write(self.dictionary[rec])
                 if not (len(string) == 0) and next_code < self.MAX_SIZE:
                     # print('the string is', string)
@@ -53,12 +46,9 @@ class LZWCompressor:
 
                     key = next_code.to_bytes(2, byteorder='little')
                     value = string + (self.dictionary[rec][0].to_bytes(1, byteorder='little'))
-                    print('will add key', key, ' (', next_code, ') with value', value)
                     self.dictionary[key] = value
                     next_code += 1
-                print('string is now', self.dictionary[rec])
                 string = self.dictionary[rec]
-                # print('string is now', string)
 
     def compress_binary_file(self, filename):
         self.reset_dictionary()
@@ -66,41 +56,29 @@ class LZWCompressor:
 
         with open(filename, "rb") as input_file, open(filename + ".lzw", "wb") as output_file:
             w = input_file.read(1)
-            print('w', w)
             while True:
                 K = input_file.read(1)
-                print('K', K)
                 if not K:
                     value = self.code(w)
                     value_bytes = value.to_bytes(2, byteorder='little')
-                    print('will write', w, 'to the file')
                     output_file.write(value_bytes)
                     break
                 wK = w + K
-                print('wK', wK)
                 if wK in self.dictionary:
-                    print('wK is in dictionary, w becomes wK')
                     w = wK
-                    print('w', w)
                 else:
-                    print('wK is not in dictionary')
                     value = self.code(w)
-                    print("retrieved code for w", w, 'will write it to compressed file')
                     value_bytes = value.to_bytes(2, byteorder='little')
-
                     output_file.write(value_bytes)
                     if len(self.dictionary) < self.MAX_SIZE:
-                        print('added wK', wK, 'to dictionary with code', self.dictionary_size)
                         self.dictionary[wK] = self.dictionary_size
                         self.dictionary_size += 1
-                    print('will assign K', K, 'to w')
                     w = K
-                    print('w', w)
+            print('size of dictionary', len(self.dictionary))
 
 
 def main():
-    print("Hello world")
-    filename = 'issue2.txt'
+    filename = 'pg100.txt'
     LZWCompressor().compress_binary_file(filename)
     compressed_file = filename + '.lzw'
     LZWCompressor().decompress_binary_file(compressed_file)
